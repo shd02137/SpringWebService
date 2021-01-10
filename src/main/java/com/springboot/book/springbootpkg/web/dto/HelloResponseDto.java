a5dab85658aae75debad600b16df8b1695ccb80c
ParkMinJo <shd02137@gmail.com>
2021-01-10 20:07:46 +0900

    스프링부트 테스트 코드 작성 및 롬북 전환

---
+++ src/main/java/com/springboot/book/springbootpkg/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.springboot.book.springbootpkg.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+
+}
